39660619ce5f85da3e996f15d46e35d798e089c9
Priyanshu-2005 <pr.agarwal2005+github@gmail.com>
2026-06-01 16:38:03 +0530

    changes made in route

--- truelens-app/src/app/api/v1/verify/route.ts
+++ truelens-app/src/app/api/v1/verify/route.ts
@@ -6,7 +6,7 @@ export async function POST(req: NextRequest) {
     const formData = await req.formData();
     
     // Fallback URL if env is not defined
-    const BACKEND_URL = process.env.BACKEND_URL || "http://127.0.0.1:8000";
+    const BACKEND_URL = process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000";
 
     const response = await fetch(`${BACKEND_URL}/api/v1/documents/verify-hash`, {
       method: "POST",
